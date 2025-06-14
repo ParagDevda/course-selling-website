b13019ba2628f42964b01f31a3ec0ebfa94b49de
ParagDevda <paragdevda@gmail.com>
2025-06-14 23:52:25 +0530

    working on it

--- frontend/script.js
+++ frontend/script.js
@@ -10,4 +10,32 @@ function user_info(n){
     }
     
 }
+
+async function getcourses(){
+    let response = await axios.get("http://localhost:3000/course/preview")
+    // console.log(response.data.courses);
+    let courses;
+    if(response){courses =response.data.courses };
+    console.log(courses)
+    let clutter="";
+    courses.forEach(element => {
+        clutter += ` <div class="cards">
+                <div id="c-upper">
+                    <img src=${element.imgUrl} alt="">
+                </div>
+                <div id="c-lower">
+                    <div id="l">
+                        <h2>${element.title}</h2>
+                        <h4> ${element.description} </h4>
+                    </div>
+                    <button>₹${element.price}</button>
+                </div>
+            </div>`;
+    });
+    document.getElementById("m-right").innerHTML =clutter;
+    console.log(clutter)
+   
+
+}
+getcourses();
 // console.log("hello")
